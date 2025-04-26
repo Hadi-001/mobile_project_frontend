03aed7cf3b29836e716bf157cfc95e99ee7fd564
adeebbm <adeebboumoghlbey@gmail.com>
2025-04-26 12:05:38 +0300

    fix agp gradle version

--- app/src/main/java/com/example/mobile_project_frontend/LoginActivity.java
+++ app/src/main/java/com/example/mobile_project_frontend/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.mobile_project_frontend;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,23 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(username.getText().toString())){
+                    username.setError("Field cannot be empty");
+                }else if(TextUtils.isEmpty(password.getText().toString())){
+                    password.setError("Field cannot be empty");
+                }else{
+                    
+                }
+            }
         });
     }
 }
